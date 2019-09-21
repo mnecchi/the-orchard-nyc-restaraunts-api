7d765de77277833a6b998ac072ed89cae56d9b5b
Massimo Necchi <massimo@akkroo.com>
2019-09-21 21:39:02 +0100

    Return total count

--- src/helpers.js
+++ src/helpers.js
@@ -13,28 +13,39 @@ const getMysqlConnection = () => new Promise((resolve, reject) => {
   })
 });
 
+const executeQuery = (connection, query) => new Promise((resolve, reject) => {
+    connection.query(query, (err, results) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        resolve(results);
+    })
+});
+
 const getQuery = ({ cuisine, minGrade, offset, limit }) => `SELECT restaurant_id, dba, boro, grade FROM restaurant
   INNER JOIN inspection ON restaurant.camis=inspection.camis AND restaurant.last_inspection_date=inspection.inspection_date
   WHERE restaurant.cuisine='${cuisine}' AND inspection.grade IN ('A', 'B', 'C', 'D', 'E', 'F') AND inspection.grade<='${minGrade}'
   LIMIT ${offset || 0},${limit || 999}`;
 
-const queryRestaurants = options => new Promise((resolve, reject) => {
-  getMysqlConnection()
-    .then(connection => {
-      connection.query(getQuery(options), (err, rows) => {
-        connection.release();
+const getCountQuery = ({ cuisine, minGrade }) => `SELECT COUNT(*) AS total_count FROM restaurant
+INNER JOIN inspection ON restaurant.camis=inspection.camis AND restaurant.last_inspection_date=inspection.inspection_date
+WHERE restaurant.cuisine='${cuisine}' AND inspection.grade IN ('A', 'B', 'C', 'D', 'E', 'F') AND inspection.grade<='${minGrade}'`;
 
-        if (err) {
-          reject(err);
-          return;
-        };
+const queryRestaurants = async options => {
+  const connection = await getMysqlConnection();
+  try {
+    const results = await executeQuery(connection, getQuery(options));
+    const totalCount = await executeQuery(connection, getCountQuery(options));
+    connection.release();
 
-        resolve(rows);
-      });
-    })
-    .catch(err => {
-      reject(err);
-    });
-});
+    const { total_count } = totalCount[0]
+    return { results, total_count };
+  } catch (err) {
+    connection.release();
+    throw new err;
+  }
+}
 
 module.exports = { queryRestaurants };
